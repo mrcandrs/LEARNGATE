68fd7e903096cd872e7bd6c2f9dc8d84eb2b778a
Marc Andres <marcandres0923@gmail.com>
2026-04-16 02:10:18 +0800

    Added dependency realignment fix

--- App.tsx
+++ App.tsx
@@ -1,5 +1,3 @@
-import "react-native-gesture-handler";
-import { StatusBar } from "expo-status-bar";
 import { PaperProvider } from "react-native-paper";
 import { AuthProvider } from "./src/store/AuthContext";
 import { RootNavigator } from "./src/navigation/RootNavigator";
@@ -9,7 +7,6 @@ export default function App() {
   return (
     <PaperProvider theme={appTheme}>
       <AuthProvider>
-        <StatusBar style="dark" />
         <RootNavigator />
       </AuthProvider>
     </PaperProvider>
